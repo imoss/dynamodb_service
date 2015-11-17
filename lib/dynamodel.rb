97058fb0dc33c40a6c6410fddcc5bc6e72c7ea2a
Ian Moss <imoss@carecloud.com>
2015-11-17 10:55:58 -0700

    fix test suite

--- lib/dynamodel.rb
+++ lib/dynamodel.rb
@@ -1,6 +1,6 @@
 require 'active_support/concern'
 require 'active_support/callbacks'
-require 'active_support/core_ext'
+require 'active_support/core_ext/module/attribute_accessors'
 
 module Dynamodel
 end
