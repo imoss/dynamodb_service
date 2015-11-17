03f565e13638a262328ac55d676c0883f7689b46
Ian Moss <imoss@carecloud.com>
2015-11-16 20:11:35 -0500

    move class methods to its own module in dynamodel document

---
+++ lib/dynamodel/document/class_methods.rb
@@ -0,0 +1,96 @@
+module Dynamodel
+  module Document
+    module ClassMethods
+      @@attribute_definitions = []
+      @@fields = []
+      @@keys = []
+      @@key_schema = []
+
+      def create(attributes={})
+        db.put_item(table_name: table_name, item: attributes)
+      end
+
+      def create_table
+        db.create_table(table_attributes)
+      end
+
+      def db
+        Aws::DynamoDB::Client.new(
+          endpoint: DynamodbService.settings.db_endpoint,
+          region: DynamodbService.settings.db_region
+        )
+      end
+
+      def delete_table
+        db.delete_table(table_name: table_name)
+      end
+
+      def field(name)
+        @@fields << name
+      end
+
+      def find(keys)
+        resp = db.get_item(table_name: table_name, key: keys)
+        new(resp.item.with_indifferent_access) if resp.item
+      end
+
+      def key(name, args={})
+        @@fields << name
+        @@keys << name
+        @@attribute_definitions << {
+          attribute_name: name,
+          attribute_type: args[:attribute_type]
+        }
+        @@key_schema << { attribute_name: name, key_type: args[:key_type] }
+      end
+
+      def table(name, args={})
+        @@table_name ||= name
+        @@read_capacity ||= args[:read_capacity]
+        @@write_capacity ||= args[:write_capacity]
+      end
+
+      def table_attributes
+        {
+          table_name: table_name,
+          attribute_definitions: attribute_definitions,
+          key_schema: key_schema,
+          provisioned_throughput: {
+            read_capacity_units: read_capacity,
+            write_capacity_units: write_capacity
+          }
+        }
+      end
+
+      # TODO: find a way to use attr_reader for getters
+
+      def attribute_definitions
+        @@attribute_definitions
+      end
+
+      def read_capacity
+        @@read_capacity
+      end
+
+      def write_capacity
+        @@write_capacity
+      end
+
+      def table_name
+        @@table_name
+      end
+
+      def fields
+        @@fields
+      end
+
+      def keys
+        @@keys
+      end
+
+      def key_schema
+        @@key_schema
+      end
+    end
+  end
+end
---
+++ spec/dynamodel/document_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+class TestDouble
+end
+
+describe Dynamodel::Document do
+end
+
